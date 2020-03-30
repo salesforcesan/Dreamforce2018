e0aa0eaf54a5a9072e64338ecd76e2541420bc8f
salesforcesan <salesforce.san@gmail.com>
2020-03-30 01:20:06 -0500

    build 1

--- build/create-scratch-org.sh
+++ build/create-scratch-org.sh
@@ -2,7 +2,7 @@
 echo "Setting up DevHub Connection..."
 mkdir keys
 echo $SFDC_SERVER_KEY | base64 -d > keys/server.key
-openssl enc -nosalt -aes-256-cbc -in server.key -out server.key.enc -base64 -K 962892A4BC3427202D133E8B803A8E2420EE6ACA5215E3296612D59C800FEB01 -iv 835C2944F866258C2DCB2D3DCDA9A81A
+openssl enc -nosalt -aes-256-cbc -in keys/server.key -out server.key.enc -base64 -K 962892A4BC3427202D133E8B803A8E2420EE6ACA5215E3296612D59C800FEB01 -iv 835C2944F866258C2DCB2D3DCDA9A81A
 
 # Authenticate to salesforce
 echo "Authenticating..."
