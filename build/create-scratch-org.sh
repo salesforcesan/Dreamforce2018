13e09fc5659a35689ade1e40e52dc71f8fa63888
salesforcesan <salesforce.san@gmail.com>
2020-03-30 01:30:25 -0500

    bi

--- build/create-scratch-org.sh
+++ build/create-scratch-org.sh
@@ -1,5 +1,8 @@
 # Get the private key from the environment variable
 echo "Setting up DevHub Connection..."
+mkdir keys
+
+openssl enc -nosalt -aes-256-cbc -in server.key -out server.key.enc -base64 -K 962892A4BC3427202D133E8B803A8E2420EE6ACA5215E3296612D59C800FEB01 -iv 835C2944F866258C2DCB2D3DCDA9A81A
 
 # Authenticate to salesforce
 echo "Authenticating..."
